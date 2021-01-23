5f3840ab718a2bf5dffb651c6555ef59cbb5eacd
sunny <sunny@jbcole.co.uk>
2021-01-23 01:57:35 +0000

    Changed name of rootReducer back to original name

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,5 +1,5 @@
 import {combineReducers} from 'redux';
 
-export const index = combineReducers({
+export const rootReducer = combineReducers({
   counter: () => 1,
 });
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import {index} from './reducers';
+import {rootReducer} from './reducers';
 
-export const store = createStore(index, applyMiddleware(thunk));
+export const store = createStore(rootReducer, applyMiddleware(thunk));
